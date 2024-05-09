2bbc88de07c665ca9a7d10ef3695b3ba9930366d
Oleksandr <o.tymoshenko@astoundcommerce.com>
2024-05-09 15:39:06 +0300

    add nginx and /etc/hosts setup

--- kubeadm-clusters/virtualbox/k8s/setup-ingress.sh
+++ kubeadm-clusters/virtualbox/k8s/setup-ingress.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 MASTER_NODE_NAME=$1 # controlplane
-DOMAIN=$2 # adminer.k8s.com
+DOMAINS=$2 # adminer.k8s.com, kanban.k8s.com
 INGRESS_NGINX_CONTROLLER_VERSION=$3 # 1.10.1
 USER="vagrant"
 
+DOMAINS=$(echo "$DOMAINS" | tr -d ' ')
 
 sudo -H -u $USER bash -c "kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v${INGRESS_NGINX_CONTROLLER_VERSION}/deploy/static/provider/cloud/deploy.yaml"
 
@@ -18,11 +19,18 @@ echo "=== Set esternal clutrer IP: $PRIMARY_IP"
 sudo chown $USER /vagrant/k8s/deployment/*
 
 # Setup /etc/hosts on VM
-echo  "127.0.0.1 $DOMAIN" | sudo tee -a /etc/hosts
+IFS=',' read -ra DOMAINS_ARRAY <<< "$DOMAINS"
+
+for DOMAIN in "${DOMAINS_ARRAY[@]}"; do
+    echo "127.0.0.1 $DOMAIN" | sudo tee -a /etc/hosts >/dev/null
+done
 
 # Setup Nginx on VM
 sudo apt install nginx -y
-echo "server {
+for DOMAIN in "${DOMAINS_ARRAY[@]}"; do
+    sudo rm -f /etc/nginx/sites-available/$DOMAIN
+    sudo rm -f /etc/nginx/sites-enabled/$DOMAIN
+    echo "server {
     listen 80;
     server_name $DOMAIN;
 
@@ -34,6 +42,7 @@ echo "server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }" | sudo tee /etc/nginx/sites-available/$DOMAIN > /dev/null
+    sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
+done
 
-sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
